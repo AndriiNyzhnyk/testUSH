eb9e566017280ab1424d038c7f44558c5316d9e1
Andrii Nyzhnyk <andrij1510@gmail.com>
2019-02-20 13:18:47 +0200

    configuration server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('port', process.env.PORT || 8000);
-
+app.use(express.static(path.resolve(__dirname, '../build')));
 const data = [
     {
         empId: 1,
@@ -37,7 +38,7 @@ const data = [
 ];
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
 app.get('/api/getDataForTable', (req, res) => {
@@ -53,6 +54,28 @@ app.delete('/api/deleteItem/:empId', (req, res) => {
     let empId = req.params['empId'];
     console.log(empId);
     res.send('ok');
+});
+
+
+app.use((req, res, next) => {
+    res.status(404).send('404!');
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('500');
+});
+
+
+process.on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at:', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
+process.on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
 });
 
 
